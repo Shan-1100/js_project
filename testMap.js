c06a5cfff6e1aa5aec08ecf8647690c43f3d9280
Shani <Shan-1100@users.noreply.github.com>
2025-05-18 07:32:13 -0400

    sun commits 2

--- testMap.js
+++ testMap.js
@@ -1,4 +1,9 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([-33.864859899365975, 151.20308604361705], 13);
+var marker = L.marker([-33.864859899365975, 151.20308604361705]).addTo(map);
+var popup = L.popup()
+    .setLatLng([-33.864859899365975, 151.20308604361705])
+    .setContent("The George Institute for Global Health, Australia")
+    .openOn(map);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
