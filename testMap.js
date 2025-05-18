cfdc19d98b3131fa5e232ed222acb8ef296c4aaf
Shani <Shan-1100@users.noreply.github.com>
2025-05-18 16:26:37 -0400

    sun pm commits2

--- testMap.js
+++ testMap.js
@@ -1,49 +1,48 @@
 
-var map = L.map('map').setView([32.78720606581554, 129.86642338802017], 13);
-var marker = L.marker([32.78720606581554, 129.86642338802017]).addTo(map);
-marker.bindPopup("<b> Graduate School of Biomedical Sciences, Nagasaki University, Japan </b><br> Site of HTLV-1 Clinical Research").openPopup();
+var map = L.map('map').setView([35.59953867565579, 139.54847218388477], 5);
+var marker1 = L.marker([35.59953867565579, 139.5484721838847]).addTo(map);
+marker1.bindPopup("<b> St. Marianna University School of Medicine, Kanagawa, Japan </b><br>Conducted multiple clinical trials").openPopup();
+var marker2 = L.marker([31.72144340048172, 130.6034920496261]).addTo(map);
+marker2.bindPopup("<b> Kagoshima University Graduate School of Medical and Dental Sciences, Kagoshima, Japan. </b><br>Conducted one clinical trial").openPopup();
+var marker2 = L.marker([31.829794259811525, 131.4152121360319]).addTo(map);
+marker2.bindPopup("<b> University of Miyazaki, Miyazaki, Japan.. </b><br>Conducted one clinical trial").openPopup();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var mapSalvador = L.map('mapSalvador').setView([-12.99267836882544, -38.51984238154737], 13);
-var markerSalvador = L.marker([-12.99267836882544, -38.51984238154737]).addTo(mapSalvador);
-markerSalvador.bindPopup("<b> Hospital Universitário Professor Edgard Santos, Salvador, Brazil</b><br>HTLV-1 studies").openPopup();
+var mapBrazil = L.map('mapBrazil').setView([-12.99267836882544, -38.51984238154737], 4);
+var markerSalvador = L.marker([-12.99267836882544, -38.51984238154737]).addTo(mapBrazil);
+markerSalvador.bindPopup("<b> Hospital Universitário Professor Edgard Santos, Salvador, Brazil</b><br>Conducted multiple clinical trials").openPopup();
+var markerBelem = L.marker([-22.778905484820832, -47.58147935703872]).addTo(mapBrazil);
+markerBelem.bindPopup("<b> Universidade Metodista de Piracicaba, Belém, Brazil</b><br>Conducted one clinical trial").openPopup();
+var markerSalvador2 = L.marker([-13.00279768527676, -38.50739491822406]).addTo(mapBrazil);
+markerSalvador2.bindPopup("<b> Universidade Federal da Bahia, Salvador, Brazil</b><br>Conducted one clinical trial").openPopup();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(mapSalvador);
+}).addTo(mapBrazil);
 
-var mapBelem = L.map('mapBelem').setView([-22.778905484820832, -47.58147935703872], 13);
-var markerBelem = L.marker([-22.778905484820832, -47.58147935703872]).addTo(mapBelem);
-markerBelem.bindPopup("<b> Universidade Metodista de Piracicaba, Belém, Brazil</b><br>HTLV-1 studies").openPopup();
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(mapBelem);
-
-var mapBethesda = L.map('mapBethesda').setView([39.0005399372308, -77.10138888148074], 13);
-var markerBethesda = L.marker([39.0005399372308, -77.10138888148074]).addTo(mapBethesda);
-markerBethesda.bindPopup("<b> National Institute of Health, Bethesda, Maryland, USA </b><br>HTLV-1 studies").openPopup();
+var mapIran = L.map('mapIran').setView([36.28305913204988, 59.543423963352154], 7);
+var markerIran = L.marker([36.28305913204988, 59.543423963352154]).addTo(mapIran);
+markerIran.bindPopup("<b> Mashhad University of Medical Sciences, Mashhad, Iran </b><br>Conducted one clinical trial").openPopup();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(mapBethesda);
+}).addTo(mapIran);
 
 const mapMultiple = L.map('mapMultiple').setView([40.406, -79.107], 6);
 const markerMultiple1 = L.marker([40.848722217858395, -73.84460536206392]).addTo(mapMultiple);
-markerMultiple1.bindPopup("<b> Montefiore, Einstien, Bronx, NY, USA: </b><br>Funded HTLV-1 clinical studies").openPopup();
+markerMultiple1.bindPopup("<b> Montefiore, Einstien, Bronx, NY, USA: </b><br>Conducted one clinical trial").openPopup();
 const markerMultiple2 = L.marker([39.0005399372308, -77.10138888148074]).addTo(mapMultiple);
-markerMultiple2.bindPopup("<b> NIH, Bethesda, MD, USA: </b><br>Funded HTLV-1 clinical studies").openPopup();
+markerMultiple2.bindPopup("<b> NIH, Bethesda, MD, USA: </b><br>Conducted multiple clinical trials").openPopup();
 const markerMultiple3 = L.marker([39.9612, -82.9988]).addTo(mapMultiple);
-markerMultiple3.bindPopup("<b> Ohio State University Comprehensive Cancer Center, OH, USA: </b><br>Funded HTLV-1 clinical studies").openPopup();
+markerMultiple3.bindPopup("<b> Ohio State University Comprehensive Cancer Center, OH, USA: </b><br>Conducted one clinical trial").openPopup();
 const markerMultiple4 = L.marker([39.328, -76.620]).addTo(mapMultiple);
-markerMultiple4.bindPopup("<b> Johns Hopkins University/Sidney Kimmel Cancer Center, MD, USA: </b><br>Funded HTLV-1 clinical studies").openPopup();
+markerMultiple4.bindPopup("<b> Johns Hopkins University/Sidney Kimmel Cancer Center, MD, USA: </b><br>Conducted one clinical trial").openPopup();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
